06d398123c1fdcf9d86546e7ce3fa678db69005d
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-04 01:11:20 +0530

    Update 28-read-or-save-final-json.py

--- model/28-read-or-save-final-json.py
+++ model/28-read-or-save-final-json.py
@@ -1,1 +1,20 @@
-28-read-or-save-final-json.ipynb
+"""
+Your Phone Can Spot Fashion – A lightweight yet powerful system that analyzes short-form videos in real time to identify fashion products by combining computer vision and natural language processing, all processed locally.
+
+Copyright (C) 2025 Aryaman Gajrani
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+Contact: arya-gaj@proton.me
+"""
