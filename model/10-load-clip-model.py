a9dc6e91abb73fd4d236f53bf39557c3692e3c91
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2025-07-05 15:55:17 +0530

    Update 10-load-clip-model.py

--- model/10-load-clip-model.py
+++ model/10-load-clip-model.py
@@ -18,3 +18,6 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 Contact: arya-gaj@proton.me
 """
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
